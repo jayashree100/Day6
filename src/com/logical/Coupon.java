3c467b90547eb5178e54e31ab3266a87ec7a4602
JAISHRI <sjayashree529@gmail.com>
2022-03-07 12:07:21 +0530

    changed

--- src/com/logical/Coupon.java
+++ src/com/logical/Coupon.java
@@ -1,36 +1,38 @@
 package com.logical;
 
+import java.util.Scanner;
+
 public class Coupon {
 	
-	public static int getCoupon(int n) {
-        return (int) (Math.random() * n);
-    }
+	      static int getCoupon(int N) {
+	          
+	    	   return (int) (Math.random() * N);
+	    }
+	
+	 	public static void main(String args[]) {
+	 		
+	 		Scanner sc = new Scanner(System.in);
+	 		System.out.println("Enter the String ");
+	 		String str = sc.next();
+	 		char[] ch = str.toCharArray();
+	 		System.out.println("Enter the coupon N value");
+	 		int N = sc.nextInt();
+	 		
+	 		int random = getCoupon(N);
+	 		System.out.println(random);
+	 			
+	 		String sb = " ";
+	 		
+			while (random > 0) {
+				sb = sb + (ch[random % ch.length]);
+				random /= ch.length;
+				System.out.println(random + "  " + random % ch.length + " sb is " + sb);
+			}
+			
+			String couponCode = sb.toString();
+			System.out.println("Coupon Code: " + couponCode);
 
-   
-    public static int collect(int n) {
-        
-    	    boolean[] isCollected = new boolean[n];  
-            int count = 0;                           
-            int distinct  = 0;     
-
-            
-        while (distinct < n) {
-            int value = getCoupon(n);            
-            count++;
-            
-             if (!isCollected[value]) {           
-                distinct++;
-                isCollected[value] = true;
-            }
-        }
-        return count;
-    }
-
-    
-    public static void main(String[] args) {
-         int n = 26;
-        int count = collect(n);
-       System.out.println(count);
-    } 
-
+	 } 
 }
+
+
